8a67efb4a1e7547b04680d834e83376609e30c88
David Greaves <david@dgreaves.com>
2012-06-06 20:03:48 +0100

    Support synchronous copyproj

--- osc/core.py
+++ osc/core.py
@@ -4875,6 +4875,7 @@ def copy_prj(src_apiurl, src_project, dst_project,
              withhistory = False,
              makeolder = False,
              resign = False,
+             now = False,
              comment = None):
     """
     Create a copy of a project.
@@ -4894,6 +4895,8 @@ def copy_prj(src_apiurl, src_project, dst_project,
         query['resign'] = '1'
     if comment:
         query['comment'] = comment
+    if now:
+        query['nodelay'] = '1'
     u = makeurl(src_apiurl, ['source', dst_project], query=query)
     print >>sys.stderr, "copyprj ", u
     f = http_POST(u)
